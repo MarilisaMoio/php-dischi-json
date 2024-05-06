6da7991b2b3ca601c43b7e7ca8d22f0cafc97004
Marilisa Moio <moiomarilisa@gmail.com>
2024-05-06 16:00:48 +0200

    chore: add function for axios calls

--- js/script.js
+++ js/script.js
@@ -3,10 +3,20 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
+            param: {
+                discIndex: null
+            }
         };
     },
     methods: {
+        getCDFromApi(){
+            axios.get("server.php", { params: this.param })
+            .then((response) => {
+                console.log(response)
+            })
+        }
     },
     mounted() {
+        this.getCDFromApi()
     }
 }).mount('#app');
