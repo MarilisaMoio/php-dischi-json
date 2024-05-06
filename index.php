8dcd02dc359b3fc27cebc134691c0901dca98c5d
Marilisa Moio <moiomarilisa@gmail.com>
2024-05-06 15:37:19 +0200

    spooky scary skeleton

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Discs</title>
+    <link rel="stylesheet" href="css/style.css">
+    <!--vue.js-->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <!--axios-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.6.8/axios.min.js"></script>
+</head>
+<body>
+    <div id="app">
+    
+    </div>
+    <!--js-->
+    <script type="text/javascript" src="js/script.js"></script>
+</body>
+</html>
